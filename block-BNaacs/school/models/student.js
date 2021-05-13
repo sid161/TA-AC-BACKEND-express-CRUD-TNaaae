76a081189e59938c820d77b81ca12e52f3df484c
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-05-13 18:14:42 +0530

    block-BNaacs

---
+++ block-BNaacs/school/models/student.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+
+var studentSchema = new Schema({
+    name: String,
+    address: String,
+    age: Number
+})
+
+var Student = mongoose.model('Student',studentSchema);
+module.exports = Student;
