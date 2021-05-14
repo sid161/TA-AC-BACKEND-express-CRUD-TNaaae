a02701543724db0f90c07bc4d5d9a6fa61668708
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-05-14 20:22:24 +0530

    block-BNaacu

--- block-BNaacu/school/app.js
+++ block-BNaacu/school/app.js
@@ -1,21 +1,31 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var path = require("path");
+const {send} = require('process');
 
-mongoose.connect('mongodb://localhost/school1', (err) => {
+mongoose.connect('mongodb://localhost/school1', ({ useUnifiedTopology: true , useUnifiedTopology: true}), (err) => {
     console.log("Database Connected");
 })
 
 var app = express()
 
+
 app.set("viewengine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use('/student',require('./routes/students.js'))
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use('/students',require('./routes/students.js'))
 
 app.get('/',(req,res) => {
     var student = {name:"sid",age:20}
     res.render('index',{student:student});
+})
+
+// handle error
+app.get((req,res,next) => {
+    res.send("Page not found");
 })
 
 app.listen(3000, () => {
--- block-BNaacu/school/routes/students.js
+++ block-BNaacu/school/routes/students.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/new',(req,res) => {
-    res.send("Form Data");
+    res.render('studentsForm.ejs');
 })
 
 router.post('/', (req,res) => {
-    res.send("Student added");
+    console.log(req.body);
+    res.send("req.body");
 })
 
 router.get('/', (req,res) => {
-    res.render('index',{list: ["ankit", "suraj","ravi", "prashant"]})
+    res.render('list.ejs',{list: ["ankit", "suraj","ravi", "prashant"]})
 })
 
 router.get('/student/:id',(req,res) => {
     res.render('studentDetail',{ student : {name:"rahul",email:"rahul@altcampus.io"}});
 })
+
+module.exports = router;
